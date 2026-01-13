225cd3e0829abc699103ae17d7635bebaf4512c5
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-01-14 01:42:52 +0530

    [Bhuvnesh Singh Bhadauriya] Refactor. Updated calculator message in addition branch

--- src/calculator/Addition.java
+++ src/calculator/Addition.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class Addition {
     public int add(int a, int b) {
-    	System.out.println("Performing addition");
+    	System.out.println("Calculator Version - ADDITION");
         return a + b;
     }
 }
